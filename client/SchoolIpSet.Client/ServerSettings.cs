e94a9cf77e3aac8e81fa6061a14d7d32fdddb5a8
Alumos <1281610535@qq.com>
2026-09-13 19:52:49 +0800

    chore: bump Windows client version to 0.1.1

--- client/SchoolIpSet.Client/ServerSettings.cs
+++ client/SchoolIpSet.Client/ServerSettings.cs
@@ -6,6 +6,6 @@ namespace SchoolIpSet.Client
         // Keep the API URL configurable in source so a release can target the school VPS.
         public const string ApiBaseUrl = "http://139.196.136.61:18080/";
         public const string ServerPublicKeyJwk = "__SERVER_PUBLIC_KEY_JWK__";
-        public const string ClientVersion = "0.1.0";
+        public const string ClientVersion = "0.1.1";
     }
 }
